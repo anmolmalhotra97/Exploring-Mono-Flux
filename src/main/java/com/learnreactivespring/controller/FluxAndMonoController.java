0faeccbf91942de2744028e7d3c602445bc50dfe
Anmol Malhotra <anmol.malhotra@cleartrip.com>
2021-05-19 02:52:25 +0530

    added Test case and controller for mono implementation

--- src/main/java/com/learnreactivespring/controller/FluxAndMonoController.java
+++ src/main/java/com/learnreactivespring/controller/FluxAndMonoController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -27,6 +28,12 @@ public class FluxAndMonoController {
     @GetMapping(value = "/fluxInfiniteStream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Long> returnFluxInfiniteStream() {
         return Flux.interval(Duration.ofSeconds(1))
+                .log();
+    }
+
+    @GetMapping(value = "/mono")
+    public Mono<Integer> returnMono() {
+        return Mono.just(1)
                 .log();
     }
 
